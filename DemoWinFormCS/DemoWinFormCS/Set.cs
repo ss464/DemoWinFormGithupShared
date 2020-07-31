58590c06d7c5fe994539d1bcd260f3f8cd432530
io.NET <io.NET@IO>
2020-07-31 13:38:27 +0200

    create conniction string

--- DemoWinFormCS/DemoWinFormCS/Set.cs
+++ DemoWinFormCS/DemoWinFormCS/Set.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
 
 namespace DemoWinFormCS
 {
     class Set
     {
+
+
+        SqlConnection cn = new SqlConnection();
     }
 }
